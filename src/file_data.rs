893e4615a1383d1c8abf0086db05488829a8465a
Sergey Zhukov <szhukov@aligntech.com>
2024-11-17 09:28:32 +0300

    fix: add file_id metadata column

--- src/file_data.rs
+++ src/file_data.rs
@@ -6,6 +6,7 @@ use datafusion::arrow::array::{Int64Array, RecordBatch, StringArray};
 use datafusion::arrow::datatypes::{DataType, Field, Schema};
 
 pub struct FileData {
+    file_id: Option<String>, 
     file_name: Option<String>, 
     file_type: Option<String>,
     file_path: Option<String>,
@@ -17,6 +18,7 @@ pub struct FileData {
 
 impl FileData {
     pub fn new(
+        file_id: Option<&str>, 
         file_name: Option<&str>, 
         file_type: Option<&str>, 
         file_path: Option<&str>, 
@@ -26,6 +28,7 @@ impl FileData {
         dt_fmt: Option<&str>,
     ) -> Self {
         Self { 
+            file_id: file_id.map(|x| x.to_string()),
             file_name: file_name.map(|x| x.to_string()), 
             file_type: file_type.map(|x| x.to_string()), 
             file_path: file_path.map(|x| x.to_string()), 
@@ -38,6 +41,7 @@ impl FileData {
 
     pub async fn to_df(ctx: SessionContext, records: &Vec<Self>) -> Result<DataFrame> {
         let schema = Schema::new(vec![
+            Field::new("file_id", DataType::Utf8, true),
             Field::new("file_name", DataType::Utf8, true),
             Field::new("file_type", DataType::Utf8, true),
             Field::new("file_size", DataType::Int64, true),
@@ -47,7 +51,8 @@ impl FileData {
             Field::new("dt_fmt", DataType::Utf8, true),
         ]);
 
-        let mut file_names = vec![];
+        let mut file_ids = vec![];
+        let mut file_names = vec![];    
         let mut file_types = vec![];
         let mut file_sizes = vec![];
         let mut file_paths = vec![];
@@ -56,6 +61,7 @@ impl FileData {
         let mut dts_fmt = vec![];
 
         for record in records {
+            file_ids.push(record.file_id.as_deref());
             file_names.push(record.file_name.as_deref());
             file_types.push(record.file_type.as_deref());
             file_sizes.push(record.file_size);
@@ -68,6 +74,7 @@ impl FileData {
         let batch = RecordBatch::try_new(
             Arc::new(schema),
         vec![
+                Arc::new(StringArray::from(file_ids)),
                 Arc::new(StringArray::from(file_names)),
                 Arc::new(StringArray::from(file_types)),
                 Arc::new(Int64Array::from(file_sizes)),
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use std::path::Path;
+
 use anyhow::Result;
 use aws_sdk_s3::Client;
 use chrono::prelude::*;
 use datafusion::prelude::*;
-use std::path::Path;
+use uuid::Uuid;
 
 pub mod config;
 pub mod utils;
@@ -21,6 +23,7 @@ pub async fn handler(client: Client, config: Config) -> Result<DataFrame> {
     println!("start processing data");
     let mut file_data_all = vec![];
     for (file, (file_size, dt)) in files {
+        let file_id = Uuid::new_v4().to_string();
         let path = Path::new(&file);
         let file_name = path.file_name().map(|x| x.to_string_lossy().to_string());
         let file_type = path.extension().map(|x| x.to_string_lossy().to_string());
@@ -35,6 +38,7 @@ pub async fn handler(client: Client, config: Config) -> Result<DataFrame> {
         };
 
         let file_data = FileData::new(
+            Some(&file_id),
             file_name.as_deref(), 
             file_type.as_deref(), 
             file_path.as_deref(), 
